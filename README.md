dbe0971920295b94c64e6e6213f98bb8507b0d96
shajahan2819 <79057457+shajahan2819@users.noreply.github.com>
2021-02-15 12:42:55 +0530

    testcases

--- README.md
+++ README.md
@@ -9,4 +9,15 @@ RestfulEMBL
 Once the application downloaded from the Git Repository, use the command prompt and go the project location, then execute the follwoing commands one by one.
 
 1.docker build -t emblrepo/restfulEMBL .
+
 2.docker run -p 8080:8080 emblrepo/restfulEMBL
+
+#Security - Basic authentication
+
+username : user (or) admin
+
+password: embl
+
+# Api Documentation
+
+http://localhost:8080/swagger-ui.html
--- src/main/java/com/embl/RestfulEmbl/model/Person.java
+++ src/main/java/com/embl/RestfulEmbl/model/Person.java
@@ -77,6 +77,55 @@ public class Person {
 	public void setFavouriteColour(String favouriteColour) {
 		this.favouriteColour = favouriteColour;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((favouriteColour == null) ? 0 : favouriteColour.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (age != other.age)
+			return false;
+		if (favouriteColour == null) {
+			if (other.favouriteColour != null)
+				return false;
+		} else if (!favouriteColour.equals(other.favouriteColour))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", age=" + age
+				+ ", favouriteColour=" + favouriteColour + "]";
+	}
 	
 
 }
